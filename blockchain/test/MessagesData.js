a41456d1a4203a7aae2f980c389c87c42276358d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-17 11:05:57 -0300

    add testing script boilerplate
    Test the deployment on the hardhat network of the contract to store the messages in the blockchain.
    - Check if the deployment address is not null, blank, nor the zero address.

---
+++ blockchain/test/MessagesData.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const { ethers } = require('hardhat')
+
+describe("Message Data Storage", function () {
+  // variables declaration
+  let messagesData
+  
+  
+  beforeEach(async () => {
+    // Setup accounts - to get signers use `const signers = await ethers.getSigners()`
+    [deployer, account01] = await ethers.getSigners()
+    
+    // Deploy MessagesData
+    const MessagesData = await ethers.getContractFactory('MessagesData')
+    messagesData = await MessagesData.connect(deployer).deploy()
+  })
+  
+  
+  it('Deployment address.', async () => {
+    const result = await messagesData.address
+    expect(result).to.not.equal(null)
+    expect(result).to.not.equal('')
+    expect(result).to.not.equal('0x')
+    expect(result).to.not.equal(ethers.constants.AddressZero)
+  })
+});
