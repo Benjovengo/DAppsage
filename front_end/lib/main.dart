f04709c5f75eb11198a74e22065c3a21bcc1c538
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-25 11:37:50 -0300

    add custom font
    Add the Futura font face to the app
    - Futura regular
    - Futura italic
    - Futura heavy
    
    The source of the fonts are located in ./fonts/ folder and the fonts are imported by 'pubspec.yaml'. The Yaml file defines the name of the family and the path to the font files.
    
    After importing by pubspec.yaml, the fonts can be used in fontFamily property of TextStyle.
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -3,11 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My first app.'),
+          title: const Text(
+            'Blockchain Twitter.',
+            style: TextStyle(
+              letterSpacing: 2.0,
+              fontFamily: 'Futura',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           centerTitle: true,
         ),
         body: const Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello, World!',
+            style: TextStyle(
+              fontSize: 20.0,
+              letterSpacing: 2.0,
+              fontFamily: 'Futura',
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton.extended(
           label: const Text("Click me"),
